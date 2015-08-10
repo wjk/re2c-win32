2940fea957ef9a7651672f8c85f9e1b83595c109
William Kent <wjk011@gmail.com>
2015-08-10 15:55:56 -0400

    Add re2c client library

---
+++ libre2c/readfd.c
@@ -0,0 +1,117 @@
+/* readfd.c
+ * Scott Bronson
+ * 28 Dec 2004
+ *
+ * Version VERSION
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <assert.h>
+#include <io.h>
+
+#include "readfd.h"
+#include "scan-dyn.h"
+
+/** @file readfd.c
+ *
+ * This file provides a readproc that reads from Unix file descriptors.
+ */
+
+
+/**
+ * The internal function that performs the read.  You never call
+ * it directly.  Instead, it is called automatically by the scanner
+ * whenever it needs more data.
+ */
+
+static ssize_t readfd_read(scanstate *ss)
+{
+    int n, avail;
+
+    avail = read_shiftbuf(ss);
+
+    // ensure we get a full read
+    do {
+        n = _read((int)ss->readref, (void*)ss->limit, avail);
+    } while(n < 0 && errno == EINTR);
+    ss->limit += n;
+
+    return n;
+}
+
+
+/** Attaches the existing fd to the existing scanstate object.
+ * Note that this routine checks the fd and if it's less than 0
+ * (indicating an error) it returns null.
+ *
+ * If you pass this routine valid arguments, there's no way for it to fail.
+ *
+ * Note that the re2c library does not work very well with file descriptors
+ * that may return short reads such as pipes and sockets.  You may want to
+ * try to use the new re2c push model to fix this.
+ */
+
+scanstate* readfd_attach(scanstate *ss, int fd)
+{
+    if(!ss || fd < 0) {
+        return 0;
+    }
+
+    ss->readref = (void*)fd;
+    ss->read = readfd_read;
+    return ss;
+}
+
+
+/* Opens the file and creates a new scanner to scan it.
+ * This is just a convenience routine.  You can create a scanner
+ * yourself and attach to it using readfd_attach().
+ *
+ * If you do use this routine, you should call readfd_close() to close
+ * the file and deallocate the scanner.
+ *
+ * Bufsiz tells how big in bytes the scan buffer will be.  No single
+ * token may be larger than bufsiz.
+ *
+ * This is the approximate equivalent to:
+ * - dynscan_create(bufsiz);
+ * - readfd_attach(ss, open(path));
+ */
+
+scanstate* readfd_open(const char *path, size_t bufsiz)
+{
+    scanstate *ss;
+    int fd;
+
+	// _open() is marked as deprecated due to security concerns; however,
+	// using the suggested function, _sopen_s(), is overkill for my needs here.
+	// Thus, I suppress the deprecation warning for this statement only.
+#pragma warning(suppress: 4996)
+    fd = _open(path, O_RDONLY);
+    if(fd < 0) {
+        return NULL;
+    }
+
+    ss = dynscan_create(bufsiz);
+    if(!ss) {
+        _close(fd);
+        return NULL;
+    }
+
+    return readfd_attach(ss, fd);
+}
+
+
+/**
+ * Closes the file and deallocates the memory allocated by readfd_open().
+ */
+
+void readfd_close(scanstate *ss)
+{
+    _close((int)ss->readref);
+    dynscan_free(ss);
+}
+
